26b190c4b21ce3be8fa7db534f8f06d38a08ac06
AKAPaneL <dpanzi@outlook.com>
2023-02-20 21:02:14 +0800

    轮播图布局

---
+++ src/pages/index/components/banner.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="banner">
+		<swiper class="swiper" indicator-dots circular autoplay :interval="interval" :duration="duration">
+			<swiper-item v-for="item in info" :key="item">
+				<view class="swiper-item">{{ item }}</view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				info:[1,2,3,4,5],
+				interval:5000,
+				duration:500
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.banner{
+		width: 100vw;
+		height: 35vw;
+        padding: 4vw 0;
+		.swiper{
+			height: 100%;
+		}
+		swiper-item{
+			box-sizing: border-box;
+			padding: 0 4vw;
+		}
+		.swiper-item{
+			display: block;
+			height: 100%;
+			background-color:#E91E63;
+            border-radius: 3vw;
+		}
+	}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,31 +1,19 @@
 <template>
 	<view class="content">
-		<view class="banner">
-			<uni-swiper-dot :info="info" mode="round" :bottom="100">
-				<swiper class="swiper" circular autoplay :interval="interval" :duration="duration">
-					<swiper-item>
-						<view class="swiper-item">A</view>
-					</swiper-item>
-					<swiper-item>
-						<view class="swiper-item">B</view>
-					</swiper-item>
-					<swiper-item>
-						<view class="swiper-item">C</view>
-					</swiper-item>
-				</swiper>
-			</uni-swiper-dot>
-		</view>
+		<!-- banner部分-->
+		<banner></banner>
 	</view>
 </template>
 
 <script>
+import banner from './components/banner.vue';
 	export default {
+		components:{
+			banner
+		},
 		data() {
 			return {
-				info:[1,2,3],
-				indicatorDots: true,
-				interval:5000,
-				duration:500
+
 			}
 		},
 		onLoad() {
@@ -43,21 +31,5 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-	.banner{
-		width: 100vw;
-		height: 45vw;
-		.swiper{
-			height: 100%;
-		}
-		swiper-item{
-			box-sizing: border-box;
-			padding: 5vw;
-		}
-		.swiper-item{
-			display: block;
-			height: 100%;
-			background-color:red;
-		}
 	}
 </style>
